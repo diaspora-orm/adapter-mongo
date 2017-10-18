0a0b2bdb2180b5311465ae051a3d3ae144a92e1d
Gerkin <nihilivin@gmail.com>
2017-10-18 03:29:30 +0200

    Removed c from checks

--- test/adapters/mongo.js
+++ test/adapters/mongo.js
@@ -4,7 +4,6 @@ const AdapterTestUtils = require( 'diaspora/test/adapters/utils' );
 const ADAPTER_LABEL = 'mongo';
 
 const adapter = AdapterTestUtils.createDataSource( ADAPTER_LABEL, {database: 'diasporaMongoTest'});
-console.log(adapter)
 adapter.waitReady().then(() => {
 	adapter.db.dropCollection('test');
 	adapter.db.dropCollection('app1-matchmail-simple');
